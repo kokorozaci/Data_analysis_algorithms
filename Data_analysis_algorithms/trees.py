27e2ca52452c0b982b8f0ee000dc8266a87a8d88
kokorozaci <anna.mark.spb@gmail.com>
2020-11-03 19:08:35 +0300

    dz_01

--- Data_analysis_algorithms/trees.py
+++ Data_analysis_algorithms/trees.py
@@ -480,7 +480,7 @@ X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_s
 #                               min_samples_split=2,
 #                               random_state=2)
 
-
+#
 tree = GradientBoostingRegressor(learning_rate=0.1,
                                  n_estimators=20,
                                  max_depth=5,
@@ -491,3 +491,99 @@ print(tree.evaluate(X_train, X_test, y_train, y_test))
 tree.error_plot()
 # print(DecisionTreeClassifier.accuracy_metric(y_train, tree.predict(X_train)))
 # print(DecisionTreeClassifier.accuracy_metric(y_test, tree.predict(X_test)))
+
+from sklearn.tree import DecisionTreeRegressor
+
+from sklearn import model_selection
+import numpy as np
+
+
+def gb_predict(X, trees_list, coef_list, eta):
+    # Реализуемый алгоритм градиентного бустинга будет инициализироваться нулевыми значениями,
+    # поэтому все деревья из списка trees_list уже являются дополнительными и при предсказании прибавляются с шагом eta
+    return np.array([sum([eta * coef * alg.predict([x])[0] for alg, coef in zip(trees_list, coef_list)]) for x in X])
+
+
+def mean_squared_error(y_real, prediction):
+    return (sum((y_real - prediction) ** 2)) / len(y_real)
+
+
+def bias(y, z):
+    return (y - z)
+
+
+def gb_fit(n_trees, max_depth, X_train, X_test, y_train, y_test, coefs, eta):
+    # Деревья будем записывать в список
+    trees = []
+
+    # Будем записывать ошибки на обучающей и тестовой выборке на каждой итерации в список
+    train_errors = []
+    test_errors = []
+
+    for i in range(n_trees):
+        tree = DecisionTreeRegressor(max_depth=max_depth, random_state=42)
+
+        # инициализируем бустинг начальным алгоритмом, возвращающим ноль,
+        # поэтому первый алгоритм просто обучаем на выборке и добавляем в список
+        if len(trees) == 0:
+            # обучаем первое дерево на обучающей выборке
+            tree.fit(X_train, y_train)
+
+            train_errors.append(mean_squared_error(y_train, gb_predict(X_train, trees, coefs, eta)))
+            test_errors.append(mean_squared_error(y_test, gb_predict(X_test, trees, coefs, eta)))
+        else:
+            # Получим ответы на текущей композиции
+            target = gb_predict(X_train, trees, coefs, eta)
+
+            # алгоритмы начиная со второго обучаем на сдвиг
+            tree.fit(X_train, bias(y_train, target))
+
+            train_errors.append(mean_squared_error(y_train, gb_predict(X_train, trees, coefs, eta)))
+            test_errors.append(mean_squared_error(y_test, gb_predict(X_test, trees, coefs, eta)))
+
+        trees.append(tree)
+
+    return trees, train_errors, test_errors
+
+
+# Число деревьев в ансамбле
+n_trees = 200
+
+# для простоты примем коэффициенты равными 1
+coefs = [1] * n_trees
+
+# Максимальная глубина деревьев
+max_depth = 3
+
+# Шаг
+eta = 0.01
+
+trees, train_errors, test_errors = gb_fit(n_trees, max_depth, X_train, X_test, y_train, y_test, coefs, eta)
+
+
+def evaluate_alg(X_train, X_test, y_train, y_test, trees, coefs, eta):
+    train_prediction = gb_predict(X_train, trees, coefs, eta)
+
+    print(f'Ошибка алгоритма из {n_trees} деревьев глубиной {max_depth} \
+    с шагом {eta} на тренировочной выборке: {mean_squared_error(y_train, train_prediction)}')
+
+    test_prediction = gb_predict(X_test, trees, coefs, eta)
+
+    print(f'Ошибка алгоритма из {n_trees} деревьев глубиной {max_depth} \
+    с шагом {eta} на тестовой выборке: {mean_squared_error(y_test, test_prediction)}')
+
+
+evaluate_alg(X_train, X_test, y_train, y_test, trees, coefs, eta)
+
+
+def get_error_plot(n_trees, train_err, test_err):
+    plt.xlabel('Iteration number')
+    plt.ylabel('MSE')
+    plt.xlim(0, n_trees)
+    plt.plot(list(range(n_trees)), train_err, label='train error')
+    plt.plot(list(range(n_trees)), test_err, label='test error')
+    plt.legend(loc='upper right')
+    plt.show()
+
+
+get_error_plot(n_trees, train_errors, test_errors)
